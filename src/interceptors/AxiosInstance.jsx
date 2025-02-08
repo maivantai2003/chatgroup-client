b389b3d0d61e05fde230c63bdf0280f641c3aeda
Van Tai <vantai08122003@gmail.com>
2025-02-08 22:44:35 +0700

    update

---
+++ src/interceptors/AxiosInstance.jsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+const axiosInstance=axios.create({
+    baseURL:`{""}/`,
+    headers: {
+        "Content-type": "application/json",
+      },
+      withCredentials: true,
+})
+//axiosInstance.interceptors.request.use()
