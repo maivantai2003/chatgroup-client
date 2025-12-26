95fb96bf143efdf6f5a4c6996063819b1f3065a1
maivantai2003 <vantai08122003@gmail.com>
2025-12-27 00:38:32 +0700

    docs(README.md): change path image

--- src/pages/NewPapers.jsx
+++ src/pages/NewPapers.jsx
@@ -1,6 +1,6 @@
 import React,{ useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { GetNewPapers } from "../redux/newPaper/newPaperSlice";
+import { GetNewPapers } from "../redux/newpaper/newPaperSlice";
 import { Spinner } from "../components/Spinner";
 
 const NewParers = () => {
